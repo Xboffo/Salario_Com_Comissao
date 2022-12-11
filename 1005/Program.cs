57b56d7a79e7f8eaed1c96c4af38e22c943778f4
Kauet Lima <kauet123@live.com>
2022-12-11 17:02:42 -0300

    Primeiro Commit

---
+++ 1005/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = Console.ReadLine();
+            double salario, totalDeVendas;
+
+
+            salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            totalDeVendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+             salario = salario + (totalDeVendas * 0.15);
+
+            Console.WriteLine("TOTAL = R$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
